9d755ed6184f2c8534860b9a8f500de88fdbef04
Sinevia <info@sinevia.com>
2019-06-17 21:00:06 +0100

    Update Registry.php

--- src/Registry.php
+++ src/Registry.php
@@ -42,14 +42,14 @@ class Registry {
         self::$registry = array();
     }
     
-    public static function get($key, $default = null) {
+    public static function get(string $key, $default = null) {
         if (self::has($key)) {
             return self::$registry[$key];
         }
         return $default;
     }
     
-    public static function has($key) {
+    public static function has(string $key) {
         return (isset(self::$registry[$key])) ? true : false;
     }
     
@@ -57,18 +57,32 @@ class Registry {
      * Checks if a key equals the given value
      * @return boolean
      */
-    public static function equals($key, $value) {
+    public static function equals(string $key, $value) {
         $currentValue = self::get($key);
         return ($currentValue === $value) ? true : false;
     }
     
-    public static function remove($key) {
+    /**
+     * Checks if a key equals any the given values
+     * @return boolean
+     */
+    public static function equalsAny(string $key, array $values) {
+        $currentValue = self::get($key);            
+        foreach($values as $value) {
+            if ($currentValue === $value) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static function remove(string $key) {
         if (self::has($key) == true) {
             unset(self::$registry[$key]);
         }
     }
 
-    public static function set($key, $value) {
+    public static function set(string $key, $value) {
         self::$registry[$key] = $value;
     }
     
@@ -76,18 +90,18 @@ class Registry {
      * Sets a key only if it does not exist
      * @return boolean
      */
-    public static function setIfNotExists($key, $value) {
+    public static function setIfNotExists(string $key, $value) {
         if (self::has($key)) {
             return;
         }
         self::$registry[$key] = $value;
     }
 
-    public static function fromArray($array) {
+    public static function fromArray(array $array) {
         self::$registry = $array;
     }
 
-    public static function mergeArray($array) {
+    public static function mergeArray(array $array) {
         self::$registry = array_merge(self::$registry, $array);
     }
 
